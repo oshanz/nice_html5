77c576caf761ea320d5c6f234f50afaaf6883acc
Oshanz <spiritualmirror@gmail.com>
2014-04-07 15:00:27 +0530

    requirejs added

---
+++ public_html/index.js
@@ -0,0 +1,16 @@
+/*
+ * @author Waruna Oshan Wisumperuma
+ * @contact warunaoshan@gmail.com
+ */
+
+(function() {
+	requirejs.config({
+		paths : {
+			'jquery' : '../libs/jquery-2.1.0.min'
+		}
+	});
+
+	require([], function() {
+		alert('adad');
+	});
+})();
