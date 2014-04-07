327c25599fbd36ae04250c14ca8f51be9e9731b8
Oshanz <spiritualmirror@gmail.com>
2014-04-07 22:56:25 +0530

    optimize

--- public_html/libs/config.js
+++ public_html/libs/config.js
@@ -5,22 +5,18 @@
 requirejs.config({
 	baseUrl : 'libs',
 	paths : {
-		jquery : 'jquery-2.1.0.min',
-		jquery_ui : 'jquery-ui/jquery-ui.min',
-		jquery_mobile : 'jquery-mobile/jquery.mobile-1.4.2.min',
-		underscore : 'underscore-min',
-		backbone : 'backbone-min'
+		'jquery' : 'jquery-2.1.0.min',
+		'jquery_ui' : 'jquery-ui/jquery-ui.min',
+		'jquery_mobile' : 'jquery-mobile/jquery.mobile-1.4.2.min',
+		'underscore' : 'underscore-min',
+		'backbone' : 'backbone-min'
 	},
 	shim : {
-		backbone : {
+		'backbone' : {
 			deps : ["underscore", "jquery"],
 			exports : "Backbone"
 		},
-		jquery_ui : {
-			deps : ["jquery"]
-		},
-		jquery_mobile : {
-			deps : ["jquery"]
-		}
+		'jquery_ui' : ["jquery"],
+		'jquery_mobile' : ["jquery"]
 	}
 });
