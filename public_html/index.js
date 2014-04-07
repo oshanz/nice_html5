c8d5c6119ccdafa77ff8e667f9b7fb191389174d
Oshanz <spiritualmirror@gmail.com>
2014-04-07 15:37:22 +0530

    shim added

--- public_html/index.js
+++ public_html/index.js
@@ -7,12 +7,24 @@
 	requirejs.config({
 		paths : {
 			'jquery' : 'libs/jquery-2.1.0.min',
-			'_' : '/libs/underscore-min',
-			'backbone' : '/libs/backbone-min'
+			'_' : 'libs/underscore-min',
+			'backbone' : 'libs/backbone-min'
+		},
+		shim : {
+			jquery : {
+				exports : '$'
+			},
+			underscore : {
+				exports : '_'
+			},
+			backbone : {
+				deps : ["underscore", "jquery"],
+				exports : "Backbone"
+			}
 		}
 	});
 
-	require(['jquery'], function($) {
+	require(['jquery'], function() {
 		$(document).ready(function() {
 			alert('aasf');
 		});
